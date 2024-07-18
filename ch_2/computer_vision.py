043fa319e4308c3c3db335399a093415fde67c62
EdCarney <carneyedwardj@gmail.com>
2024-07-18 09:58:48 -0400

    Adds CV examples

---
+++ ch_2/computer_vision.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+data = tf.keras.datasets.fashion_mnist
+
+(training_imgs, training_lbls), (test_imgs, test_lbls) = data.load_data()
+
+# normalize the test data values to 0-1
+training_imgs = training_imgs / 255.0
+test_imgs = test_imgs / 255.0
+
+model = tf.keras.models.Sequential([
+        # optional shape dimension for the data
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+
+        # middle layer; every node takes in all 184 (28*28) values
+        # activation function is the logic that executes on each node
+        # RELU (rectified linear unit) just returns a value if > 0
+        tf.keras.layers.Dense(128, activation=tf.nn.relu),
+
+        # output layer; one node for each category
+        # we only want the value from the node with the highest value
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+# ADAM optimizer is an improvement on SGD (stochastic gradient descent)
+# the loss function is specified as one of the 'categorical' set
+# we also add a metric here to report on; there are a defined set of these
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# train the model
+model.fit(training_imgs, training_lbls, epochs=5)
+
+# check the model's accuracy
+model.evaluate(test_imgs, test_lbls)
+
+# now let's get some info on one of the pieces of test data
+# note that classifications will return an array of output node values
+classifications = model.predict(test_imgs)
+print(classifications[0])
+print(test_lbls[0])
---
+++ ch_2/computer_vision_overfitting.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+# here we do the same thing as for computer_vision, but now we run our
+# training for 10x the number of epochs, leading to some noticiable overfitting
+
+data = tf.keras.datasets.fashion_mnist
+
+(training_imgs, training_lbls), (test_imgs, test_lbls) = data.load_data()
+
+training_imgs = training_imgs / 255.0
+test_imgs = test_imgs / 255.0
+
+model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_imgs, training_lbls, epochs=50)
+
+model.evaluate(test_imgs, test_lbls)
+
+classifications = model.predict(test_imgs)
+print(classifications[0])
+print(test_lbls[0])
