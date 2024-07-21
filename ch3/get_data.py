ce0f356c892e5e9ebe3686d2a50e6108ca0d35d0
EdCarney <carneyedwardj@gmail.com>
2024-07-21 18:14:55 -0400

    Adds functions to get horses and humans data for local testing

--- ch3/get_data.py
+++ ch3/get_data.py
@@ -4,38 +4,38 @@ import urllib.request
 import zipfile
 
 BASE_URL = "https://storage.googleapis.com/learning-datasets/"
+TRAINING_DIR = "horse-or-human/training/"
 TRAINING_ZIP = "horse-or-human.zip"
 VALIDATION_ZIP = "validation-horse-or-human.zip"
+VALIDATION_DIR = "horse-or-human/validation/"
 
 
 def download_training_data():
     url = BASE_URL + TRAINING_ZIP
-    dir = 'horse-or-human/training/'
 
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
+    if os.path.exists(TRAINING_DIR):
+        shutil.rmtree(TRAINING_DIR)
 
     print("Downloading data from", url)
     urllib.request.urlretrieve(url, TRAINING_ZIP)
 
     with zipfile.ZipFile(TRAINING_ZIP, 'r') as zip_ref:
-        zip_ref.extractall(dir)
+        zip_ref.extractall(TRAINING_DIR)
 
     os.remove(TRAINING_ZIP)
 
 
 def download_validation_data():
     url = BASE_URL + VALIDATION_ZIP
-    dir = 'horse-or-human/validation/'
 
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
+    if os.path.exists(VALIDATION_DIR):
+        shutil.rmtree(VALIDATION_DIR)
 
     print("Downloading data from", url)
     urllib.request.urlretrieve(url, VALIDATION_ZIP)
 
     with zipfile.ZipFile(VALIDATION_ZIP, 'r') as zip_ref:
-        zip_ref.extractall(dir)
+        zip_ref.extractall(VALIDATION_DIR)
 
     os.remove(VALIDATION_ZIP)
 
