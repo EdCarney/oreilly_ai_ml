97e9ccfd2d54169777e4ea6b5406c9d51a964b72
EdCarney <carneyedwardj@gmail.com>
2024-07-21 18:15:22 -0400

    Extracts CNN model from the compilation logic

--- ch3/cnn.py
+++ ch3/cnn.py
@@ -1,5 +1,16 @@
 ﻿import tensorflow as tf
 
+# CONVOLUTION LAYER
+# we define the number of convolutions we want the model to learn as 64;
+# (3, 3) is the filter size, i.e. the number of pixels that will be part
+# of the convolution; we define the input shape initially, and the
+# activation function per usual; we want the model to learn what the best
+# filter values are for these convolutions
+
+# MAX POOLING LAYER
+# this groups ('pools') 2x2 sets of pixels together and takes the max, this
+# has the affect of reducing the number of pixels by 4
+
 
 class CnnModel:
     def __init__(self):
@@ -18,5 +29,32 @@ class CnnModel:
         return self._model
 
     model: tf.keras.Sequential = property(
-        fget = _get_model
+        fget=_get_model
+    )
+
+
+class HorseHumanCnnModel:
+    def __init__(self):
+        self._model = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(16, (3, 3), activation='relu',
+                                   input_shape=(300, 300, 3)),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid')
+            ])
+
+    def _get_model(self) -> tf.keras.Sequential:
+        return self._model
+
+    model = property(
+        fget=_get_model
     )
--- ch3/compile_cnn.py
+++ ch3/compile_cnn.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from cnn import  CnnModel
+from cnn import CnnModel
 
 data = tf.keras.datasets.fashion_mnist
 
@@ -16,21 +16,10 @@ test_img = test_img.reshape(10_000, 28, 28, 1)
 training_img = training_img / 255.0
 test_img = test_img / 255.0
 
-# CONVOLUTION LAYER
-# we define the number of convolutions we want the model to learn as 64;
-# (3, 3) is the filter size, i.e. the number of pixels that will be part
-# of the convolution; we define the input shape initially, and the
-# activation function per usual; we want the model to learn what the best
-# filter values are for these convolutions
-
-# MAX POOLING LAYER
-# this groups ('pools') 2x2 sets of pixels together and takes the max, this
-# has the affect of reducing the number of pixels by 4
-
 cnn_model = CnnModel()
 cnn_model.model.compile(optimizer=tf.optimizers.Adam(),
-              loss=tf.losses.SparseCategoricalCrossentropy(),
-              metrics='accuracy')
+                        loss=tf.losses.SparseCategoricalCrossentropy(),
+                        metrics='accuracy')
 
 cnn_model.model.fit(training_img, training_lbl, epochs=50)
 
