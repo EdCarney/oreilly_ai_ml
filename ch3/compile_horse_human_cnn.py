2a24ea425ff4e4834a0c83785d99b1d8d499f6ad
EdCarney <carneyedwardj@gmail.com>
2024-07-21 18:21:07 -0400

    Adds comments to horse human training model

--- ch3/compile_horse_human_cnn.py
+++ ch3/compile_horse_human_cnn.py
@@ -3,6 +3,22 @@ from cnn import HorseHumanCnnModel
 from get_data import TRAINING_DIR, VALIDATION_DIR
 
 
+# if we want to generate our data from our own local sources, we can use an
+# image data generator; this will allow us to have data in a format like
+#   data_dir
+#   |---- training
+#   |--------  category_1
+#   |--------  category_2
+#   |--------  category_3
+#   |---- validation
+#   |--------  category_1
+#   |--------  category_2
+#   |--------  category_3
+
+# note that the validation data is meant to be used DURING the training as
+# an indicator for how well the model is doing; this is different from
+# training data that is meant to be used only AFTER the model is trained
+
 # we want the image pixel values to be normalized b/w 0 and 1
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
