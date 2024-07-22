cbcbb6a9e70578e18990a7881aee8a4383fbb72b
EdCarney <carneyedwardj@gmail.com>
2024-07-21 22:48:50 -0400

    Makes horse-human CNN functions to load data from a dataset exportable

--- ch3/compile_horse_human_cnn.py
+++ ch3/compile_horse_human_cnn.py
@@ -19,27 +19,42 @@ from get_data import TRAINING_DIR, VALIDATION_DIR
 # an indicator for how well the model is doing; this is different from
 # training data that is meant to be used only AFTER the model is trained
 
-# we want the image pixel values to be normalized b/w 0 and 1
-train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+# note that we want the image pixel values to be normalized b/w 0 and 1
 
-train_generator = train_datagen.flow_from_directory(
-        directory=TRAINING_DIR,
-        target_size=(300, 300),
-        class_mode='binary')
-validation_generator = validation_datagen.flow_from_directory(
-        directory=VALIDATION_DIR,
-        target_size=(300, 300),
-        class_mode='binary')
+def get_horse_human_train_datagen():
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
 
-cnn_model = HorseHumanCnnModel()
-model: tf.keras.Sequential = cnn_model.model
-model.compile(
-        loss='binary_crossentropy',
-        optimizer=tf.optimizers.RMSprop(learning_rate=0.001),
-        metrics=['accuracy'])
+    train_generator = train_datagen.flow_from_directory(
+            directory=TRAINING_DIR,
+            target_size=(300, 300),
+            class_mode='binary')
 
-history = model.fit_generator(
-        train_generator,
-        epochs=15,
-        validation_data=validation_generator)
+    return train_generator
+
+
+def get_horse_human_validation_datagen():
+    validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255)
+
+    validation_generator = validation_datagen.flow_from_directory(
+            directory=VALIDATION_DIR,
+            target_size=(300, 300),
+            class_mode='binary')
+
+    return validation_generator
+
+
+if __name__ == "__main__":
+
+    cnn_model = HorseHumanCnnModel()
+    model: tf.keras.Sequential = cnn_model.model
+    model.compile(
+            loss='binary_crossentropy',
+            optimizer=tf.optimizers.RMSprop(learning_rate=0.001),
+            metrics=['accuracy'])
+
+    history = model.fit_generator(
+            get_horse_human_train_datagen(),
+            epochs=15,
+            validation_data=get_horse_human_validation_datagen())
