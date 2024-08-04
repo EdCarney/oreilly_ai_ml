b1d28016ad550d2c146e5969cc7937e8f3585ce4
EdCarney <carneyedwardj@gmail.com>
2024-08-04 18:26:32 -0400

    Refactors stopwords to be in a constants file

--- ch5_intro_to_nlp/imdb_processing_with_preprocessing.py
+++ ch5_intro_to_nlp/imdb_processing_with_preprocessing.py
@@ -2,23 +2,7 @@ import string
 import tensorflow as tf
 import tensorflow_datasets as tfds
 from bs4 import BeautifulSoup
-
-# these are common words that don't mean much and could negatively impact
-# model training, so we remove them
-
-stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at",
-             "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do",
-             "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having",
-             "he", "hed", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how",
-             "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "it", "its", "itself",
-             "lets", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought",
-             "our", "ours", "ourselves", "out", "over", "own", "same", "she", "shed", "shell", "shes", "should",
-             "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then",
-             "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through",
-             "to", "too", "under", "until", "up", "very", "was", "we", "wed", "well", "were", "weve", "were",
-             "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why",
-             "whys", "with", "would", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself",
-             "yourselves"]
+from constants import STOPWORDS
 
 # we create a translation table to translate common punctuation and remove
 # it from sentences
@@ -52,7 +36,7 @@ for item in train_data:
         word = word.translate(table)
 
         # remove the stopwords
-        if word not in stopwords:
+        if word not in STOPWORDS:
             filtered_sentence += word + " "
 
     imdb_sentences.append(filtered_sentence)
