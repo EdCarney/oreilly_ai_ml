668312fd5aeefa21f90c23cb4d3a07c2acbb683d
EdCarney <carneyedwardj@gmail.com>
2024-08-06 22:22:18 -0400

    Adds more examples of model modification

---
+++ ch6_sentiment_and_embeddings/02_03_modifying_the_sarcasm_model_vocab_size.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+from sarcasm_data_processing import get_sarcasm_sequences_and_labels
+
+# note that if you plot a histogram of the word count in the corpus
+# you can see the vast majority (~80%) of the words occur less than
+# 20 times; this can mean a lot of noise in the data; limiting our
+# vocab size can help to prevent this
+
+VOCAB_SIZE = 2_000
+
+(train_seqs, train_lbls), (test_seqs, test_lbls) =\
+        get_sarcasm_sequences_and_labels(VOCAB_SIZE)
+
+train_seqs = np.array(train_seqs)
+train_lbls = np.array(train_lbls)
+test_seqs = np.array(test_seqs)
+test_lbls = np.array(test_lbls)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Embedding(VOCAB_SIZE, 16),
+    tf.keras.layers.GlobalAvgPool1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+    ])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(), metrics=['acc'])
+
+print(model.summary())
+
+model.fit(train_seqs, train_lbls, epochs=100,
+          validation_data=(test_seqs, test_lbls))
+model.evaluate(test_seqs, test_lbls)
--- ch6_sentiment_and_embeddings/02_04_modifying_the_sarcasm_model_dimensions.py
+++ ch6_sentiment_and_embeddings/02_04_modifying_the_sarcasm_model_dimensions.py
@@ -2,9 +2,18 @@ import numpy as np
 import tensorflow as tf
 from sarcasm_data_processing import get_sarcasm_sequences_and_labels
 
+# a dimension size of 16 was arbitrary; as a rule of thumb, the dimensionality
+# of the vector space should be the fourth-root of the vocab size; so for 2000
+# we are looking at a size of about 7
+#
+# we will see that the results are similar to the previous training, but the
+# loss for the validation set stabilizes quickly and allows for faster, more
+# regular training
+
+VOCAB_SIZE = 2_000
 
 (train_seqs, train_lbls), (test_seqs, test_lbls) =\
-        get_sarcasm_sequences_and_labels(10_000)
+        get_sarcasm_sequences_and_labels(VOCAB_SIZE)
 
 train_seqs = np.array(train_seqs)
 train_lbls = np.array(train_lbls)
@@ -12,7 +21,7 @@ test_seqs = np.array(test_seqs)
 test_lbls = np.array(test_lbls)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Embedding(10_000, 16),
+    tf.keras.layers.Embedding(VOCAB_SIZE, 7),
     tf.keras.layers.GlobalAvgPool1D(),
     tf.keras.layers.Dense(24, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid'),
